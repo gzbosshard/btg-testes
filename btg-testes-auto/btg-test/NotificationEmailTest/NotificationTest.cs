7792e7be984726ac0eef402347da927edd9f7151
Gabriela Zanotto Bosshard <gzbosshard@gmail.com>
2023-12-11 18:53:57 -0300

    implementação de testes em notification

---
+++ btg-testes-auto/btg-test/NotificationEmailTest/NotificationTest.cs
@@ -0,0 +1,86 @@
+﻿using btg_testes_auto.Notification;
+using btg_testes_auto.Order;
+using FluentAssertions;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace btg_test.NotificationEmailTest
+{
+    public class NotificationTest
+    {
+        private readonly IEmailService _mockEmailService;
+        private NotificationService _service; // _sut system under test
+
+        public NotificationTest()
+        {
+            _mockEmailService = Substitute.For<IEmailService>();
+            _service = new(_mockEmailService);
+        }
+
+        [Fact]
+        public void SendNotification_MessageNull_ReturnFalse()
+        {
+            //Arrange
+            string recipient = null;
+            string message = null;
+
+            //Act
+            bool result = _service.SendNotification(recipient, message);
+
+            //Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void SendNotification_MessageEmpty_ReturnFalse()
+        {
+            //Arrange
+            string recipient = null;
+            string message = " ";
+
+            //Act
+            bool result = _service.SendNotification(recipient, message);
+
+            //Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void SendNotification_SendEmail_EmailSent()
+        {
+            //Arrange
+            string recipient = null;
+            string message = "message";
+
+            _mockEmailService.SendEmail(recipient, "Notification", message)
+                .Returns(true);
+
+            //Act
+            bool result = _service.SendNotification(recipient, message);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void SendNotification_Esception_ReturnFalse()
+        {
+            //Arrange
+            string recipient = null;
+            string message = "message";
+
+            _mockEmailService.SendEmail(recipient, "Notification", message)
+                .Returns(false);
+
+            //Act
+            bool result = _service.SendNotification(recipient, message);
+
+            //Assert
+            result.Should().BeFalse();
+        }
+    }
+}
